5237f8d4b685f4b04bdfcd3e7bb585e6c0942679
steve-leo254 <steveleo254@gmail.com>
2023-06-16 22:28:15 +0300

    mysales

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask ,render_template
+from main2 import insert_product,fetch_data
+
+#create an object called app
+#__name__ is used to tell flask where to access html files
+#all htmls files are put inside templates folder
+#all css/js/images are put inside static folder
+app = Flask(__name__)
+
+#A route is an extension of urlwhich loads you an html page
+#techcamp.co.ke/
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index")
+def about():
+    #LOAD RECORS FROM DB HERE
+    return "front page"
+
+
+@app.route('/products')
+def products ():
+    prods =fetch_data("products")
+    return render_template('products.html', prods=prods)
+
+app.run()
